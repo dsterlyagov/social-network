2935698bfc12d76f5fe02c6a9a4bd2098b37f5cb
demetrius <dsterlyagov@gmail.com>
2020-09-14 20:05:47 +0300

    lesson 45 connect react-redux

--- src/components/Profile/MyPosts/MyPostsContainer.js
+++ src/components/Profile/MyPosts/MyPostsContainer.js
@@ -1,36 +1,29 @@
 import React from "react";
-import StoreContext from "../../../StoreContext";
+
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 
-const MyPostsContainer = (props) => {
 
-    return (
-        <StoreContext.Consumer>
-            {
-            (store) => {
-              //  let state = props.store.getState();
-                let addPost = () => {
-                    store.dispatch(addPostActionCreator())
-                }
-
-                let onPostChange = (text) => {
-                    let action = updateNewPostTextActionCreator(text);
-                    store.dispatch(action);
-                }
-                return <MyPosts updateNewPostText={onPostChange}
-                                addPost={addPost}
-                                posts={store.getState().profilePage.posts}
-                                newPostText={store.getState().profilePage.newPostText}
-                />
-            }
+import {connect} from "react-redux";
 
 
-        }
-
-        </StoreContext.Consumer>
-
-    )
-
+const mapStateToProps = (state) =>{
+    return{
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText,
+    }
 }
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        updateNewPostText: (text) =>{
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        },
+        addPost: () =>{
+            dispatch(addPostActionCreator());
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 export default MyPostsContainer;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './redux/redux-store';
-import StoreContext from "./StoreContext";
-import {Provider} from "./StoreContext";
+
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
  let rerenderEntireTree = (state) =>{
 
