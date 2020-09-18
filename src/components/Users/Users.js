9eacdab569b883f83c5ce8b7e03309dfa23f6a42
demetrius <dsterlyagov@gmail.com>
2020-09-18 10:44:08 +0300

    lesson 51 - connect rest api

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,20 @@
 import React from "react";
 import classes from './Users.module.css';
+import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
     if(props.users.length ===0){
-        props.setUsers([
-            {id:1 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
-            {id:2 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
-            {id:3 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss too too', location: {city: 'Kiev', country: 'Ukraine'}},
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response=>{
 
-        ])
+                props.setUsers(response.data.items)})
+        // props.setUsers([
+        //     {id:1 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
+        //     {id:2 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
+        //     {id:3 , photoUrl: 'https://img.afisha.tut.by/static/media/340x0s/people/0f/b/dmitriy-nagiev-027054.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss too too', location: {city: 'Kiev', country: 'Ukraine'}},
+        //
+        // ])
     }
 
     return <div>
@@ -16,7 +22,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={classes.userPhoto}/>
+                        <img src={u.photos.small !=null ? u.photos.small : userPhoto} className={classes.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -29,11 +35,11 @@ let Users = (props) => {
                          </div>
                 </span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
-                <span><div>{u.location.country}</div>
-                <div>{u.location.city}</div></span>
+                <span><div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div></span>
             </div>)
         }
     </div>
