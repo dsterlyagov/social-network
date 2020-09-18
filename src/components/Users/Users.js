b151a58852f2ac19d0d830f3bf31702e08d9b66e
demetrius <dsterlyagov@gmail.com>
2020-09-18 12:38:44 +0300

    lesson 54 add circle live componentDidMount

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,24 +2,23 @@ import React, {Component} from "react";
 import classes from './Users.module.css';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user.png'
-class Users extends Component{
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
 
-                    this.props.setUsers(response.data.items)
-                })
-        }
+class Users extends Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+
+                this.props.setUsers(response.data.items)
+            })
     }
+
     render() {
-          return <div>
-            <button onClick={this.getUsers}>Get users</button>
+        return <div>
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small !=null ? u.photos.small : userPhoto} className={classes.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={classes.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
